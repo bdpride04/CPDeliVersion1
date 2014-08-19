a9c5752b7b2b95d221dc9b66b48a100dfb43f4d4
bdpride04 <emilkhan@Guests-MacBook.local>
2014-08-19 19:32:16 -0400

    added files (emil khan)

---
+++ CPDeliVersion1/CreateAccount/CreateAccountTests/CreateAccountTests.m
@@ -0,0 +1,34 @@
+//
+//  CreateAccountTests.m
+//  CreateAccountTests
+//
+//  Created by Emil Khan on 8/12/14.
+//  Copyright (c) 2014 emil khan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CreateAccountTests : XCTestCase
+
+@end
+
+@implementation CreateAccountTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
